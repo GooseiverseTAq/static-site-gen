7b6397fd1272fdb72321da8afdd6888c71d22ddc
Matthew Ruenes <matthewruenes@Matthews-MacBook-Air.local>
2026-02-08 12:35:53 -0600

    Added Static Copying

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
-from textnode import TextNode, TextType
-def main():
-    node = TextNode(
-        "This is some text",
-        TextType.TEXT,
-    )
-    print(node)
+import os
+import shutil
 
-if __name__ == "__main__":
-    main()
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
